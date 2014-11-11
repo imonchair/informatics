cc09b142b9acecb117d0dfb4bad7554d6ff57f14
imonchair <jack.gifford2264@gmail.com>
2014-11-11 20:38:27 +1100

    fixed it a little. its getting there!

--- ninja/ninja.py
+++ ninja/ninja.py
@@ -1,18 +1,19 @@
 fin = open("ninjain.txt", 'r')
 fout = open("ninjaout.txt", 'w')
 
-numNinja, sneakPast = fin.readline().split()
-totalSneakPast = 0
-currentNinja = 0
-limit = 0
+ninjaStart, sneakPast = fin.readline().split()
+
+ninjaEvasion = 0
 sneakPast = int(sneakPast)
-numNinja = int(numNinja)
-while currentNinja <= numNinja:
-	while limit < 3:
-		if currentNinja < numNinja:
-			totalSneakPast += 1
-			limit += 1
-		else:
-			fout.write(str(totalSneakPast))
-	currentNinja += 1
-fout.write(str(totalSneakPast))
+ninjaStart = int(ninjaStart)
+
+while ninjaStart > 0:
+	ninjaStart += -1
+	while ninjaEvasion < 3:
+		if ninjaStart != 1:
+			sneakPast += 1
+			ninjaStart += -1
+		ninjaEvasion += 1
+
+
+fout.write(str(sneakPast))
