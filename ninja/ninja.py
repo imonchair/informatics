7a74860b6e8e1c46e20bcffac31a1597e52f327a
jack <jack.gifford2264@gmail.com>
2014-11-11 20:11:53 +1100

    modified some of the stuff please dont hate me

--- ninja/ninja.py
+++ ninja/ninja.py
@@ -4,12 +4,15 @@ fout = open("ninjaout.txt", 'w')
 numNinja, sneakPast = fin.readline().split()
 totalSneakPast = 0
 currentNinja = 0
+limit = 0
 sneakPast = int(sneakPast)
 numNinja = int(numNinja)
 while currentNinja <= numNinja:
-	currentNinja += sneakPast + 1
-	totalSneakPast += sneakPast
-totalSneakPast += -1
+	while limit < 3:
+		if currentNinja < numNinja:
+			totalSneakPast += 1
+			limit += 1
+		else:
+			fout.write(str(totalSneakPast))
+	currentNinja += 1
 fout.write(str(totalSneakPast))
-fin.close()
-fout.close()
