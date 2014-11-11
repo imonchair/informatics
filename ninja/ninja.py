952ceac3f025477261160bdc8f60239e682efd01
imonchair <jack.gifford2264@gmail.com>
2014-11-11 22:21:41 +1100

    Getting there
    Not quite sure what the issue is

--- ninja/ninja.py
+++ ninja/ninja.py
@@ -6,14 +6,20 @@ ninjaStart, sneakPast = fin.readline().split()
 ninjaEvasion = 0
 sneakPast = int(sneakPast)
 ninjaStart = int(ninjaStart)
+totalSneakPast = 0
 
-while ninjaStart > 0:
+while ninjaStart > 1:
 	ninjaStart += -1
-	while ninjaEvasion < 3:
-		if ninjaStart != 1:
-			sneakPast += 1
-			ninjaStart += -1
-		ninjaEvasion += 1
+	if ninjaStart > sneakPast:
+		totalSneakPast += sneakPast
+		ninjaStart += -(sneakPast)
+	if ninjaStart <= sneakPast:
+		totalSneakPast += 1
+		ninjaStart += -1
 
 
-fout.write(str(sneakPast))
+
+fout.write(str(totalSneakPast))
+
+
+#write it so that if ninjastart is less than sneak past it corrects
